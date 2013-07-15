3e67b8977d0a1d46a9d6419f9b0b60a346906b20
navaneethkn <navaneethkn@gmail.com>
2013-07-15 20:55:59 +0530

    Setup and start working with jruby

---
+++ scripts/launcher.rb
@@ -0,0 +1,11 @@
+
+puts $LOAD_PATH
+require 'calabash-cucumber/launcher'
+
+
+
+@calabash_launcher = Calabash::Cucumber::Launcher.new
+unless @calabash_launcher.calabash_no_launch?
+	@calabash_launcher.relaunch
+    @calabash_launcher.calabash_notify(self)
+end
--- src/calabash/java/CalabashRunner.java
+++ src/calabash/java/CalabashRunner.java
@@ -1,18 +1,14 @@
 package calabash.java;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileFilter;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FilenameFilter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
 
-import xmlwise.Plist;
+import org.jruby.embed.PathType;
+import org.jruby.embed.ScriptingContainer;
 
 /**
  * Manages setting up calabash framework and launching the simulator
@@ -22,8 +18,7 @@ public final class CalabashRunner {
 
 	private final File pbxprojFile;
 	private final File projectDir;
-	private final File xcodeProjectDir;
-	private final String projectName;
+	private final ScriptingContainer container;
 	private static CalabashServerVersion serverVersion;
 
 	/**
@@ -68,28 +63,56 @@ public final class CalabashRunner {
 
 		this.projectDir = projectPath;
 		this.pbxprojFile = projectFile;
-		this.xcodeProjectDir = xcodeProjectDir;
-		this.projectName = xcodeProjectDir.getName().replace(".xcodeproj", "");
+		xcodeProjectDir.getName().replace(".xcodeproj", "");
+		
+		this.container = new ScriptingContainer();
+		initializeScriptingContainer();
+	}
+	
+	private final void initializeScriptingContainer() {
+		HashMap<String, String> environmentVariables = new HashMap<String, String>();
+		environmentVariables.put("PROJECT_DIR", projectDir.getAbsolutePath());
+		environmentVariables.put("HOME", System.getProperty("user.home"));
+		container.setEnvironment(environmentVariables);
+		
+		// Load paths points to the gem directory
+		container.getLoadPaths().addAll(getLoadPaths());
+	}
+
+	private List<String> getLoadPaths() {
+		ArrayList<String> loadPaths = new ArrayList<String>();
+		File basePath = new File("/Users/navaneeth/.rvm/gems/ruby-1.9.3-p194@tmp/gems");
+		File[] gems = basePath.listFiles(new FileFilter() {
+			
+			@Override
+			public boolean accept(File arg0) {
+				return arg0.isDirectory();
+			}
+		});
+		
+		for (File gem : gems) {
+			File libPath = new File(gem, "lib");
+			loadPaths.add(libPath.getAbsolutePath());
+		}
+		
+		return loadPaths;
 	}
 
 	/**
 	 * Setup calabash for the current project. This needs to be called only
-	 * once. If project is already setup, this function is a no-op
+	 * once.
 	 * 
 	 * @throws CalabashException
 	 */
 	public void setupCalabash() throws CalabashException {
 		if (this.pbxprojFile == null)
 			throw new CalabashException("Project path is not set");
-
-		if (isCalabashSetup()) {
+		
+		if (isCalabashSetup())
 			return;
-		}
-
-		ensureXCodeIsNotRunning();
-		backupProjectFile();
-		extractCalabashFramework();
-		injectCalabashFramework();
+		
+		container.setArgv(new String[]{"setup", projectDir.getAbsolutePath()});
+		container.runScriptlet(PathType.ABSOLUTE, "/Users/navaneeth/.rvm/gems/ruby-1.9.3-p194@tmp/gems/calabash-cucumber-0.9.151/bin/calabash-ios");
 	}
 
 	/**
@@ -102,115 +125,15 @@ public final class CalabashRunner {
 	public Application start() throws CalabashException {
 		if (!isCalabashSetup())
 			setupCalabash();
-
-		String appPath = null;
-		try {
-			appPath = findAppBundlePath();
-		} catch (AutoDetectAppBundlePathException e) {
-			throw new CalabashException(
-					String.format(
-							"Can't find the application bundle path. Please build '%s-cal' target from Xcode.\nIf your Xcode build points to non-standard location, set APP_BUNDLE_PATH environment variable to the application path",
-							projectName));
-		}
-
-		// Checking if application is already running. If yes, kill it before we
-		// launch again
-		// This is required because Calabash server need to be restarted after
-		// each run
-		Application existing = new Application();
-		if (existing.isRunning())
-			existing.exit();
-
-		final String[] cmd = {
-				"/Users/navaneeth/projects/calabash/calabash-ios-java/deps/ios-sim",
-				"launch", appPath, "--sdk", "6.1", "--family", "iphone",
-				"--exit" };
-		try {
-			Runtime.getRuntime().exec(cmd);
-		} catch (IOException e) {
-			throw new CalabashException(String.format(
-					"Unable to launch simulator. %s", e.getMessage()), e);
-		}
-
-		ensureConnectivity();
+		
+		container.clear();
+		container.runScriptlet(PathType.ABSOLUTE, "/Users/navaneeth/projects/calabash/calabash-ios-java/scripts/launcher.rb");
 		serverVersion = new Http(Config.endPoint()).getServerVersion();
-
 		return new Application();
 	}
 
 	public static CalabashServerVersion getServerVersion() {
 		return serverVersion;
-	}
-
-	private void ensureConnectivity() throws CalabashException {
-		final int MAXIMUM_RETRIES = 30;
-		int tries = 0;
-		boolean connected = false;
-
-		Http http = new Http(Config.endPoint());
-		while (!connected) {
-			if (++tries == MAXIMUM_RETRIES)
-				throw new CalabashException(
-						String.format(
-								"Can't establish connection to '%s'.\nMake sure you don't have a firewall blocking the traffic",
-								Config.endPoint()));
-
-			try {
-				connected = http.tryPing();
-			} catch (Exception ex) {
-				// We don't care this
-			} finally {
-				if (!connected)
-					sleepQuietly(1000);
-			}
-		}
-	}
-
-	private void sleepQuietly(long ms) {
-		try {
-			Thread.sleep(ms);
-		} catch (InterruptedException e) {
-		}
-	}
-
-	private void ensureXCodeIsNotRunning() throws CalabashException {
-		String[] cmd = { "sh", "-c",
-				"ps x -o command | grep 'Contents/MacOS/Xcode' | grep -v grep" };
-
-		try {
-			Process process = Runtime.getRuntime().exec(cmd);
-			int exitcode = process.waitFor();
-			if (exitcode == 0)
-				// Above command exits with 0 when there is a match non zero
-				// where there is no match
-				throw new CalabashException(
-						"Xcode is running. Please close Xcode before doing the setup");
-		} catch (IOException e) {
-			throw new CalabashException("Error checking Xcode status", e);
-		} catch (InterruptedException e) {
-			throw new CalabashException("Error checking Xcode status", e);
-		}
-	}
-
-	private void backupProjectFile() throws CalabashException {
-		File backupfile = new File(pbxprojFile.getAbsolutePath() + "."
-				+ System.currentTimeMillis() + ".bak");
-		FileInputStream in = null;
-		FileOutputStream out = null;
-		try {
-			backupfile.createNewFile();
-			in = new FileInputStream(pbxprojFile);
-			out = new FileOutputStream(backupfile);
-			int c;
-			while ((c = in.read()) != -1) {
-				out.write(c);
-			}
-			in.close();
-			out.close();
-		} catch (IOException e) {
-			throw new CalabashException("Can't make a backup of project file",
-					e);
-		}
 	}
 
 	private boolean isCalabashSetup() throws CalabashException {
@@ -231,170 +154,4 @@ public final class CalabashRunner {
 
 		return false;
 	}
-
-	private void extractCalabashFramework() throws CalabashException {
-		// TODO: remove full path and read from bundle
-		String[] cmd = {
-				"unzip",
-				"/Users/navaneeth/projects/calabash/calabash-ios-java/deps/calabash.framework.zip",
-				"-d", projectDir.getAbsolutePath() };
-		try {
-			Process process = Runtime.getRuntime().exec(cmd);
-			int exitcode = process.waitFor();
-
-			if (exitcode != 0) {
-				BufferedReader reader = new BufferedReader(
-						new InputStreamReader(process.getErrorStream()));
-				String line = null;
-				StringBuilder builder = new StringBuilder();
-				while ((line = reader.readLine()) != null) {
-					builder.append(line);
-				}
-				reader.close();
-				throw new CalabashException(
-						String.format(
-								"Error extracting calabash framework. %s. Process returned %d",
-								builder.toString(), exitcode));
-			}
-		} catch (IOException e) {
-			throw new CalabashException("Error extracting calabash framework",
-					e);
-		} catch (InterruptedException e) {
-			throw new CalabashException("Error extracting calabash framework",
-					e);
-		}
-	}
-
-	private void injectCalabashFramework() throws CalabashException {
-		String[] cmd = {
-				"/Users/navaneeth/projects/calabash/calabash-ios-java/deps/CalabashSetup",
-				xcodeProjectDir.getAbsolutePath(), projectName };
-
-		try {
-			Process process = Runtime.getRuntime().exec(cmd);
-			int exitcode = process.waitFor();
-			if (exitcode != 0) {
-				BufferedReader reader = new BufferedReader(
-						new InputStreamReader(process.getErrorStream()));
-				String line = null;
-				StringBuilder builder = new StringBuilder();
-				while ((line = reader.readLine()) != null) {
-					builder.append(line);
-				}
-				reader.close();
-				throw new CalabashException(
-						String.format(
-								"Error injecting calabash framework. %s. Process returned %d",
-								builder.toString(), exitcode));
-			}
-		} catch (IOException e) {
-			throw new CalabashException("Error injecting calabash framework", e);
-		} catch (InterruptedException e) {
-			throw new CalabashException("Error injecting calabash framework", e);
-		}
-	}
-
-	// Tries to detect the app bundle path. Usually Xcode builds to
-	// ~Library/Developer/Xcode/DerivedData
-	// Throws exception when fails
-	private String findAppBundlePath() throws AutoDetectAppBundlePathException {
-		File xcodeBuildDir = new File(
-				new File(System.getProperty("user.home")),
-				"Library/Developer/Xcode/DerivedData");
-		Collection<File> plistFiles = getDirectoryContentsRecursive(
-				xcodeBuildDir, new FilenameFilter() {
-					@Override
-					public boolean accept(File dir, String name) {
-						return name.contains("info.plist");
-					}
-				});
-
-		File buildDir = null;
-		for (File file : plistFiles) {
-			if (file.isDirectory())
-				continue;
-
-			try {
-				Map<String, Object> map = Plist.load(file);
-				Object workspacePath = map.get("WorkspacePath");
-				if (workspacePath != null
-						&& workspacePath.toString().contains(
-								projectDir.getAbsolutePath())) {
-					buildDir = file.getParentFile();
-					break;
-				}
-			} catch (Exception e) {
-			}
-		}
-
-		if (buildDir == null)
-			throw new AutoDetectAppBundlePathException();
-
-		ArrayList<File> allFilesInBuildDir = getDirectoryContentsRecursive(
-				buildDir, new FilenameFilter() {
-					@Override
-					public boolean accept(File dir, String name) {
-						return name.equals(String.format("%s.app", projectName));
-					}
-				});
-
-		// Uses otool and checking if calabash server is embedded in it
-		for (File appFile : allFilesInBuildDir) {
-			String path = appFile.getAbsolutePath();
-			if (hasCalabashFramework(path)) {
-				return appFile.getAbsolutePath();
-			}
-		}
-		throw new AutoDetectAppBundlePathException();
-	}
-
-	private boolean hasCalabashFramework(String path) {
-		if (!path.endsWith("/"))
-			path += "/";
-
-		String[] cmd = {
-				"sh",
-				"-c",
-				String.format(
-						"otool %s* -o 2> /dev/null | grep CalabashServer", path) };
-
-		try {
-			Process process = Runtime.getRuntime().exec(cmd);
-			int exitcode = process.waitFor();
-			if (exitcode == 0) {
-				return true;
-			}
-		} catch (Exception e) {
-		}
-
-		return false;
-	}
-
-	private static ArrayList<File> getDirectoryContentsRecursive(File dir,
-			FilenameFilter filter) {
-		ArrayList<File> files = new ArrayList<File>();
-		if (!dir.isDirectory())
-			return files;
-
-		File[] allFiles = dir.listFiles();
-		for (File file : allFiles) {
-			if (filter != null) {
-				boolean shouldAccept = filter.accept(dir, file.getName());
-				if (shouldAccept)
-					files.add(file);
-			} else
-				files.add(file);
-
-			if (file.isDirectory()) {
-				files.addAll(getDirectoryContentsRecursive(file, filter));
-			}
-		}
-
-		return files;
-	}
-
-	private class AutoDetectAppBundlePathException extends Exception {
-		private static final long serialVersionUID = 4983508335141990708L;
-	}
-
 }
