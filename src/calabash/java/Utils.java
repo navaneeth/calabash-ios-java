5234f6894943dc2cbf43fa55de58251ba1b6aad8
navaneethkn <navaneethkn@gmail.com>
2013-07-19 15:41:35 +0530

    Removing unwanted methods from Utils

--- src/calabash/java/Utils.java
+++ src/calabash/java/Utils.java
@@ -4,8 +4,6 @@
 package calabash.java;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,10 +13,6 @@ import org.jruby.RubyArray;
 import org.jruby.RubyHash;
 import org.jruby.RubyObject;
 
-/**
- * 
- *
- */
 final class Utils {
 
 	public static String getStringFromHash(RubyHash target, String key) {
@@ -52,7 +46,7 @@ final class Utils {
 		return String.format("%c%s", Character.toUpperCase(string.charAt(0)),
 				string.substring(1, string.length()));
 	}
-	
+
 	public static void unzip(File zipFile, File destination)
 			throws CalabashException {
 		if (!zipFile.exists())
@@ -112,60 +106,13 @@ final class Utils {
 		// new Http(Config.endPoint()).post("play", postData.toString());
 	}
 
-	public static String loadPlaybackData(String recordingName)
-			throws CalabashException {
-		// String os = Config.getOS();
-		// if (os == null) {
-		// os = "ios"
-		// + CalabashRunner.getServerVersion().getiOSVersion().major();
-		// }
-		//
-		// File file = getEventFile(recordingName, os);
-		// if (!file.exists() && "ios6".equals(os))
-		// file = getEventFile(recordingName, "ios5");
-		//
-		// if (!file.exists())
-		// throw new CalabashException(String.format(
-		// "Can't load playback data. %s does not exists",
-		// file.getAbsolutePath()));
-		//
-		// try {
-		// return readFileAsString(file);
-		// } catch (IOException e) {
-		// throw new CalabashException(String.format(
-		// "Can't load playback data from %s. %s",
-		// file.getAbsolutePath(), e.getMessage()), e);
-		// }
-		return null;
-	}
-
-	private static File getEventFile(String recordingName, String os) {
-		// TODO: Remove hardcoded paths
-		File eventsDir = new File(
-				"/Users/navaneeth/projects/calabash/calabash-ios-java/events");
-		return new File(eventsDir, String.format("%s_%s_%s.base64",
-				recordingName, os, Config.getDevice()));
-	}
-
-	private static String readFileAsString(File f) throws IOException {
-		if (!f.exists())
-			return null;
-
-		FileInputStream stream = new FileInputStream(f);
-		byte[] b = new byte[stream.available()];
-		stream.read(b);
-		stream.close();
-
-		return new String(b, "UTF-8");
-	}
-
 	public static void sleep(int ms) {
 		try {
 			Thread.sleep(10);
 		} catch (InterruptedException e) {
 		}
 	}
-	
+
 	public static Object[] toJavaArray(RubyArray array) {
 		ArrayList<Object> result = new ArrayList<Object>();
 		for (int i = 0; i < array.size(); i++) {
@@ -173,26 +120,26 @@ final class Utils {
 			Object javaObject = toJavaObject(rubyObject);
 			result.add(javaObject);
 		}
-		
+
 		return result.toArray();
 	}
-	
+
 	public static Object toJavaObject(Object rubyObject) {
 		if (rubyObject == null)
 			return rubyObject;
-		
+
 		if (rubyObject instanceof RubyArray)
 			return toJavaArray((RubyArray) rubyObject);
 		if (rubyObject instanceof RubyHash)
 			return toJavaHash((RubyHash) rubyObject);
 		if (rubyObject instanceof RubyObject)
 			return ((RubyObject) rubyObject).toJava(Object.class);
-		
+
 		return rubyObject.toString();
 	}
 
-	public static Map<?,?> toJavaHash(RubyHash rubyHash) {
-		HashMap<Object,Object> map = new HashMap<Object, Object>();
+	public static Map<?, ?> toJavaHash(RubyHash rubyHash) {
+		HashMap<Object, Object> map = new HashMap<Object, Object>();
 		Set<?> keySet = rubyHash.keySet();
 		for (Object rubyKey : keySet) {
 			Object rubyValue = rubyHash.get(rubyKey);
